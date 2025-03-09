4f7a04f90ddf5ac95f465b2bf6cd140144dc9f03
Pawan <imrealpawan@gmail.com>
2025-03-09 20:33:15 +0530

    first question

---
+++ revision.py
@@ -0,0 +1,9 @@
+# 1 Write a program to check whether a number is even or odd.
+def odd_even(n):
+    if (n // 2)*2 == n:
+        print("The number is even")
+    else:
+        print("The number is  odd")
+
+
+odd_even(8)
