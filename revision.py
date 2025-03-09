bf27528b4243a77e36eda99c50788d81cac4a823
Pawan <imrealpawan@gmail.com>
2025-03-09 21:22:56 +0530

    secong q

--- revision.py
+++ revision.py
@@ -6,4 +6,28 @@ def odd_even(n):
         print("The number is  odd")
 
 
-odd_even(8)
+# odd_even(8)
+
+# 2  Write a program to determine if a number is prime.
+
+def prime(n):
+    if n< 2:
+        return False
+    for i in range(2 , int(n**0.5)+1):
+        if n % i == 0 :
+            return False
+    return True
+
+num = int(input("Enter the number : "))
+if prime(num):
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
+
+
+
+
+
+
+
+
